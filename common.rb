94499acd8cef9c6e59439eceaa5493e6465e2afe
unknown <rodolfcs@gmail.com>
2023-10-02 06:15:26 -0300

    common

---
+++ common.rb
@@ -0,0 +1,21 @@
+
+common_here = "my word is this thing!"
+puts str.count "word"
+
+
+common_time = "Hello word"
+puts str.count "Hello"
+
+def character
+    character = Hash.new
+    puts common_time
+end 
+
+common_time.delete('c-o')
+common_time.match('word')
+
+
+
+
+
+puts common_time
